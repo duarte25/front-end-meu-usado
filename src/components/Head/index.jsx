7b5698f65a1890c5d84791a42a1555a200d9c98b
gustavo <duarte.guga2025@gmail.com>
2024-02-12 19:27:04 -0400

    Feito quando clica em um menu ele ficara com uma barra embaixo

--- src/components/Head/index.jsx
+++ src/components/Head/index.jsx
@@ -1,20 +1,32 @@
-import Styles from "./Styles.module.css"
+import React, { useEffect } from "react";
+import Styles from "./Styles.module.css";
 
 export default function Cabecalho() {
+    useEffect(() => {
+        const currentPage = window.location.pathname;
+        const menuItems = document.querySelectorAll(`.${Styles.cabecalho} a`);
+        menuItems.forEach(item => {
+            if (item.getAttribute("href") === currentPage) {
+                item.classList.add(Styles.active); // Adicionando a classe do CSS Module
+            }
+        });
+    }, []); // Usando um array vazio para garantir que este efeito só seja executado uma vez
+
     return (
         <div className={Styles.cabecalho}>
-            <img src="Logo.png"/>
+            <img src="Logo.png" className={Styles.imgLogo} alt="Logo" />
             <ul>
-                <li className={Styles.home}>
-                    <a href="/home">HOME</a>
-                    <div className={Styles.barraHome}></div>
-                </li>
-                <li><a href="/formulario">MODELOS</a></li>
+                <li><a href="/home">HOME</a></li>
+                <li><a href="/modelos">MODELOS</a></li>
                 <li><a href="/formulario">SERVIÇOS</a></li>
                 <li><a href="/formulario">SOBRE-NOS</a></li>
                 <li><a href="/formulario">CONTATO</a></li>
                 <li><a href="/formulario">NOVIDADES</a></li>
             </ul>
+            <div className={Styles.divLogin}>
+                <img src="Login.png" className={Styles.imgLogin} alt="Login" />
+                <a href="/login">ENTRAR</a>
+            </div>
         </div>
-    )
+    );
 }
---
+++ src/pages/modelos/index.jsx
@@ -0,0 +1,14 @@
+import Styles from "./Styles.module.css";
+import Head from "@/components/Head";
+
+export default function Modelo() {
+  return (
+    <>
+    <div className={Styles.fundo}>
+      <div>
+        <Head/>
+      </div>
+    </div>
+    </>
+  )
+}
